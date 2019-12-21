e71dd68dda63cb35a9e6cc1495436fb7001971a2
christopher tate <chris29tate2@hotmail.co.uk>
2019-12-21 15:16:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,11 @@
 katz_deli = []
  
- take_a_number(katz_deli)
- ("Ada")
+ take_a_number(katz_deli
+ "Ada")
+ 
+ take_a_number(katz_deli "Grace")
+ 
+ take_a_number katz_deli "Kent")
+ 
+ line(katz_deli)
+ 
